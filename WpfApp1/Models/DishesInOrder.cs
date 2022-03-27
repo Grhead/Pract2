e03c2de7c74b54d8fef0b27e3dad87b4a77622c7
Grenka <64643423+Grhead@users.noreply.github.com>
2022-03-27 22:48:09 +0700

    Восстановлены файлы модели. Работа проведена из дома. Пересоздан класс контекста

---
+++ WpfApp1/Models/DishesInOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using WpfApp1.ViewModels;
+namespace WpfApp1
+{
+    public partial class DishesInOrder : ViewModels.StaticViewModel
+    {
+        public int id;
+        public int Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+                OnPropertyChanged();
+            }
+        }
+        public int OrderId { get; set; }
+        public int DishId { get; set; }
+
+        public Dish Dish { get; set; } = null!;
+        public Order Order { get; set; } = null!;
+    }
+}
---
+++ WpfApp1/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfApp1
+{
+    public partial class Role
+    {
+        public Role()
+        {
+            Clients = new HashSet<Client>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+
+        public virtual ICollection<Client> Clients { get; set; }
+    }
+}
